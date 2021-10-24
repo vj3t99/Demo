a7bcdd0f517476f2145f2f952804c14261af076a
vianviet <vj3t99@gmail.com>
2021-10-24 20:28:18 +0700

    first

---
+++ src/main/java/com/demo/Application.java
@@ -0,0 +1,21 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@SpringBootApplication
+public class Application {
+
+	@RequestMapping("/")
+    @ResponseBody
+    String home() {
+      return "Hello World!";
+    }
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
