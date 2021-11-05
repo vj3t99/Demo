3efb062bdcbde617a723121869e1de4fcdd08820
vianviet <vj3t99@gmail.com>
2021-11-06 04:28:10 +0700

    n comit

---
+++ src/main/java/com/testing/testing/TestingApplication.java
@@ -0,0 +1,16 @@
+package com.testing.testing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@SpringBootApplication
+public class TestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingApplication.class, args);
+	}
+
+}
